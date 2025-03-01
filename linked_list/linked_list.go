ea12f8d2a836bb3215d3c5a4f1eb7f422d5202e9
Egor Chernakov <chernakov.eg@gmail.com>
2025-03-01 22:23:20 +0300

    linked list update

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -23,5 +23,8 @@ func (l *LinkedList[T]) InsertAt(int)
 func (l *LinkedList[T]) Remove(T) T
 func (l *LinkedList[T]) RemoveAt(int) T
 func (l *LinkedList[T]) Append(T)
-func (l *LinkedList[T]) Prepend(T)
+func (l *LinkedList[T]) Prepend(elem T) {
+	n := node[T]{elem, l.root}
+	l.root = &n
+}
 func (l LinkedList[T]) Get(int) T
